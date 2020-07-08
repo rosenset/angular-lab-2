d545982c4a052207919d14f48b1c0f309850e2db
rosenset <61257952+rosenset@users.noreply.github.com>
2020-07-08 19:30:41 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialPostsComponent } from './components/social-posts/social-posts.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialPostsComponent,
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
